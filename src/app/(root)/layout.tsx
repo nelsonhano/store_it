2921477861cad48ff4be5013b25e258c6d604a76
koinyan nelson <koinyannelson0@gmail.com>
2024-12-10 14:01:41 +0100

    ignore build error

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -6,6 +6,8 @@ import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
 
+export const dynamic = "force-dynamic";
+
 export default async function Layout({
   children,
 }: {
